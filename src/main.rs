0b852ec5781f30ab3b440f0f895e324e77d117e8
Andre Stemmet <andre.stemmet@jci.com>
2018-09-14 12:41:57 +0100

    First Trial

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 #![feature(uniform_paths)]
 
+use journald::reader::*;
+use journald::JournalEntry;
+
 fn main() {
-    println!("Hello World");
+    let journal_reader_config = JournalReaderConfig::default();
+    let mut journal_reader =
+        JournalReader::open(&journal_reader_config).expect("Failed to open journal");
+    journal_reader
+        .seek(JournalSeek::Tail)
+        .expect("Failed to seek to end of journal");
+    let current_entry =  journal_reader
+        .previous_entry() 
+        .expect("Failed to get previous record").unwrap();
+
+    println!("{:#?}", current_entry);
 }
