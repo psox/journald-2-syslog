4b1230d643d9516b6d3ff8739aeef76e951e8659
psox <psox[at]st8l.com>
2018-09-17 16:46:59 +0100

    Checkpoint 20180917T154649Z

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 #![feature(uniform_paths)]
 
+use config::{Config, File as ConfigFile, FileFormat, Value as ConfigValue};
 use journald::reader::{JournalReader, JournalReaderConfig, JournalSeek};
 use serde_json::{to_string_pretty as pretty, Map as JsonMap, Value as JsonValue};
 
-fn main() {
+fn get_configs(default_config_paths: Vec<String>) -> Config {
     let default_yaml_config = include_str!("defaults.yaml");
+    let mut config = Config::default();
+    config
+        .merge(ConfigFile::from_str(default_yaml_config, FileFormat::Yaml))
+        .unwrap();
+    if default_config_paths.len() > 0 {
+        config
+            .set("config_paths", ConfigValue::from(default_config_paths))
+            .unwrap();
+    }
+    config
+}
+
+fn get_args() {}
+
+fn main() {
+    //let config_paths = vec![];
+    let config_paths = vec!["/some/path".to_string()];
+    let config = get_configs(config_paths);
+    println!("{:#?}", config);
     let journal_reader_config = JournalReaderConfig::default();
     let mut journal_reader =
         JournalReader::open(&journal_reader_config).expect("Failed to open journal");
