0664c7887354c21d1ffc8a481718521d10cc5f54
psox <psox[at]st8l.com>
2018-10-26 13:49:37 +0100

    Checkpoint 20181026T124931Z

--- src/main.rs
+++ src/main.rs
@@ -109,7 +109,7 @@ use systemd::journal::{
 };
 
 type Result<T,> = StdResult<T, FailError,>;
-type InitialTuple = (CursorRecord, mpsc::SyncSender<CursorRecord,>, Config,);
+type InitialTuple = (CursorRecord, Config,);
 
 #[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone)]
 struct CursorRecord
@@ -143,7 +143,7 @@ fn send_json_to_remote_host(
       {
          loop
          {
-            let entry_result = journal_entry.recv_timeout(StdDuration::from_millis(1235,),);
+            let entry_result = journal_entry.recv_timeout(StdDuration::from_millis(7654,),);
             match entry_result
             {
                Ok((value, cursor,),) =>
@@ -155,15 +155,23 @@ fn send_json_to_remote_host(
                      Ok((),) => cursor_sender.send(cursor,).unwrap_or_default(),
                      _ =>
                      {
-                        thread::sleep(StdDuration::from_millis(1235,),);
-                        panic!("Network Connection Dropped!")
+                        break;
                      },
                   }
                },
                _ =>
                {
                   thread::sleep(StdDuration::from_millis(1235,),);
-                  continue;
+               },
+            }
+            let io_error = stream.take_error();
+            match io_error
+            {
+               Ok(..) => continue,
+               Err(error,) =>
+               {
+                  eprintln!("{:#?}", error);
+                  break;
                },
             }
          }
@@ -173,36 +181,20 @@ fn send_json_to_remote_host(
 
 fn read_write_cursor_thread(
    path : &str,
-   cursor_sender : &mpsc::SyncSender<CursorRecord,>,
    cursor_receiver : &mpsc::Receiver<CursorRecord,>,
 )
 {
    let mut pit = StdInstant::now();
-   let mut yaml_string = String::default();
-   let mut local_cursor_value : CursorRecord = yaml_from_str(&yaml_string,).unwrap_or_default();
    let mut written_cursor_value = CursorRecord::default();
-   {
-      let mut cursor_file = OpenOptions::new()
-         .read(true,)
-         .write(true,)
-         .create(true,)
-         .open(&path,)
-         .unwrap_or_else(|error| panic!("{:#?}\nwhile trying to open file: {}", error, &path),);
-      cursor_file.read_to_string(&mut yaml_string,).unwrap();
-      cursor_sender.send(local_cursor_value.clone(),).unwrap();
-   }
    // Open cursor file
    loop
    {
-      if let Ok(cursor,) = cursor_receiver.recv_timeout(StdDuration::from_millis(1235,),)
+      if let Ok(local_cursor_value,) = cursor_receiver.recv()
       {
-         local_cursor_value = cursor;
-
          if pit.elapsed() > StdDuration::from_millis(1234,)
             && written_cursor_value != local_cursor_value
          {
             let mut cursor_file = OpenOptions::new()
-               .read(true,)
                .write(true,)
                .create(true,)
                .open(&path,)
@@ -532,9 +524,6 @@ fn initialize_the_environment() -> Result<InitialTuple,>
    let config = get_configs(command_line_args,)?;
    let verbose = config.get_int("verbose",).unwrap_or(0,);
    let mut local_cursor_value = CursorRecord::default();
-   let cursor_location_file = config.get_str("last-cursor-location",)?;
-   let (cursor_exists_sender, cursor_exists_receiver,) = mpsc::sync_channel::<CursorRecord,>(7,);
-   let (cursor_value_sender, cursor_value_receiver,) = mpsc::sync_channel::<CursorRecord,>(300,);
 
    if verbose >= 5
    {
@@ -562,161 +551,146 @@ fn initialize_the_environment() -> Result<InitialTuple,>
 
       failure::bail!("Done");
    }
-
-   thread::spawn(move || {
-      read_write_cursor_thread(
-         cursor_location_file.as_str(),
-         &cursor_exists_sender,
-         &cursor_value_receiver,
-      )
-   },);
-
-   // Seek to an appropriate postion if the cursor is not set
-   if let Ok(cursor,) = cursor_exists_receiver.recv()
+   let mut journal = Journal::open(JournalFiles::All, false, false,)?;
+   match config.get_str("history-type",)?.as_str()
    {
-      local_cursor_value = cursor;
-   }
-   if local_cursor_value == CursorRecord::default()
-   {
-      let mut journal = Journal::open(JournalFiles::All, false, false,)?;
-      match config.get_str("history-type",)?.as_str()
+      "duration" =>
       {
-         "duration" =>
+         let duration = Duration::from_std(parse_duration(
+            config.get_str("history-duration",)?.as_str(),
+         )?,)?;
+
+         if verbose > 1
          {
-            let duration = Duration::from_std(parse_duration(
-               config.get_str("history-duration",)?.as_str(),
-            )?,)?;
+            eprintln!(" .. Seek Duration: {:?}", duration);
+         }
 
-            if verbose > 1
-            {
-               eprintln!(" .. Seek Duration: {:?}", duration);
-            }
-
-            if duration != Duration::seconds(0,)
-            {
-               let now : DateTime<Utc,> = Utc::now();
-
-               let start_time : u64 = now
-                  .checked_sub_signed(duration,)
-                  .unwrap()
-                  .timestamp()
-                  .to_string()
-                  .parse::<u64>()?
-                  * 1_000_000;
-
-               journal.seek(JournalSeek::ClockRealtime {
-                  usec : start_time,
-               },)?;
-            }
-            else
-            {
-               journal.seek(JournalSeek::Tail,)?;
-            }
-         },
-         "absolute" =>
+         if duration != Duration::seconds(0,)
          {
-            let absolute = config
-               .get_str("history-absolute",)?
-               .as_str()
-               .parse::<DateTime<Utc,>>()?
+            let now : DateTime<Utc,> = Utc::now();
+
+            let start_time : u64 = now
+               .checked_sub_signed(duration,)
+               .unwrap()
                .timestamp()
                .to_string()
                .parse::<u64>()?
                * 1_000_000;
 
-            if verbose > 1
-            {
-               eprintln!(" .. Seek Absolute: {:?}", absolute);
-            }
-
             journal.seek(JournalSeek::ClockRealtime {
-               usec : absolute,
+               usec : start_time,
             },)?;
-         },
-         "count" =>
+         }
+         else
          {
-            let count : i64 = config.get_int("history-count",)?;
+            journal.seek(JournalSeek::Tail,)?;
+         }
+      },
+      "absolute" =>
+      {
+         let absolute = config
+            .get_str("history-absolute",)?
+            .as_str()
+            .parse::<DateTime<Utc,>>()?
+            .timestamp()
+            .to_string()
+            .parse::<u64>()?
+            * 1_000_000;
 
-            if verbose > 1
+         if verbose > 1
+         {
+            eprintln!(" .. Seek Absolute: {:?}", absolute);
+         }
+
+         journal.seek(JournalSeek::ClockRealtime {
+            usec : absolute,
+         },)?;
+      },
+      "count" =>
+      {
+         let count : i64 = config.get_int("history-count",)?;
+
+         if verbose > 1
+         {
+            eprintln!(" .. Seek Records: {:?}", count);
+         }
+
+         if count > 0
+         {
+            journal.seek(JournalSeek::Head,)?;
+            // Make sure we are at the begining
+            loop
             {
-               eprintln!(" .. Seek Records: {:?}", count);
+               if let Ok(None,) = journal.previous_record()
+               {
+                  break;
+               }
             }
 
-            if count > 0
+            for _ in 0 .. count
             {
-               journal.seek(JournalSeek::Head,)?;
-               // Make sure we are at the begining
-               loop
+               if let Ok(None,) = journal.next_record()
                {
-                  if let Ok(None,) = journal.previous_record()
-                  {
-                     break;
-                  }
-               }
-
-               for _ in 0 .. count
-               {
-                  if let Ok(None,) = journal.next_record()
-                  {
-                     break;
-                  }
-               }
-
-               (0 .. count).for_each(|_| {
-                  journal.next_record().unwrap();
-               },);
-            }
-            else if count < 0
-            {
-               journal.seek(JournalSeek::Tail,)?;
-               // Tail does not always go to the end
-               loop
-               {
-                  if let Ok(None,) = journal.next_record()
-                  {
-                     break;
-                  }
-               }
-
-               for _ in count .. 0
-               {
-                  if let Ok(None,) = journal.previous_record()
-                  {
-                     break;
-                  }
+                  break;
                }
             }
-            else
+
+            (0 .. count).for_each(|_| {
+               journal.next_record().unwrap();
+            },);
+         }
+         else if count < 0
+         {
+            journal.seek(JournalSeek::Tail,)?;
+            // Tail does not always go to the end
+            loop
             {
-               journal.seek(JournalSeek::Tail,)?;
-               // Tail does not always go to the end
-               loop
+               if let Ok(None,) = journal.next_record()
                {
-                  if let Ok(None,) = journal.next_record()
-                  {
-                     break;
-                  }
+                  break;
                }
             }
-         },
-         history_type => panic!("{} is not a valid history-type!", history_type),
-      }
-      local_cursor_value.position = journal.cursor()?;
+
+            for _ in count .. 0
+            {
+               if let Ok(None,) = journal.previous_record()
+               {
+                  break;
+               }
+            }
+         }
+         else
+         {
+            journal.seek(JournalSeek::Tail,)?;
+            // Tail does not always go to the end
+            loop
+            {
+               if let Ok(None,) = journal.next_record()
+               {
+                  break;
+               }
+            }
+         }
+      },
+      history_type => panic!("{} is not a valid history-type!", history_type),
    }
+   local_cursor_value.position = journal.cursor()?;
    if verbose > 1
    {
-      eprintln!(" ++ Initial Cursor: {}", local_cursor_value.position);
+      eprintln!(" ++ Calculated Cursor: {}", local_cursor_value.position);
    }
-   Ok((local_cursor_value, cursor_value_sender, config,),)
+   Ok((local_cursor_value, config,),)
 }
 
 fn main_wrapper() -> Result<(),>
 {
-   let (init_cursor, cursor_value_sender, config,) = initialize_the_environment()?;
+   let (init_cursor, config,) = initialize_the_environment()?;
    let mut local_cursor_value = init_cursor;
    let verbose = config.get_int("verbose",).unwrap_or(0,);
+   let cursor_location_file = config.get_str("last-cursor-location",)?;
    let (json_value_sender, json_value_receiver,) =
       mpsc::sync_channel::<(JsonValue, CursorRecord,),>(300,);
+   let (cursor_value_sender, cursor_value_receiver,) = mpsc::sync_channel::<CursorRecord,>(300,);
    let mut old_mem_value = 0;
    if verbose >= 3
    {
@@ -754,6 +728,33 @@ fn main_wrapper() -> Result<(),>
                send_json_to_remote_host(&remote_host, &json_value_receiver, &cursor_value_sender,)
             },);
 
+            // Make sure the file handle goes out of scope
+            {
+               if let Ok(mut cursor_file,) = OpenOptions::new()
+                  .read(true,)
+                  .open(&cursor_location_file.as_str(),)
+               {
+                  let mut yaml_string = String::default();
+                  cursor_file
+                     .read_to_string(&mut yaml_string,)
+                     .unwrap_or_default();
+                  let file_cursor : CursorRecord = yaml_from_str(&yaml_string,).unwrap_or_default();
+                  // overwrite the cursor value if a record exists in the state file
+                  if file_cursor != CursorRecord::default()
+                  {
+                     local_cursor_value = file_cursor;
+                     if verbose >= 3
+                     {
+                        eprintln!(" ++ Using Cursor: {}", local_cursor_value.position);
+                     }
+                  }
+               }
+            }
+
+            thread::spawn(move || {
+               read_write_cursor_thread(cursor_location_file.as_str(), &cursor_value_receiver,)
+            },);
+
             let mut journal = Journal::open(JournalFiles::All, false, false,)?;
             journal
                .seek(JournalSeek::Cursor {
@@ -761,12 +762,12 @@ fn main_wrapper() -> Result<(),>
                },)
                .unwrap_or_default();
             let mut sleep_count = 0i64;
-            for loop_count in 1 .. 1_000_000
+            for loop_count in 1 .. 100_000
             {
                // need to do this because journald does not cleanup after itself
-               if verbose >= 3
+               if verbose >= 3 && loop_count % 1_000 == 0
                {
-                  if loop_count % 10000 == 0
+                  if loop_count % 10_000 == 0
                   {
                      eprintln!(" <> Loop/Sleep {}/{}", loop_count, sleep_count);
                      eprintln!(" ++ Cursor: {}", local_cursor_value.position);
@@ -802,7 +803,7 @@ fn main_wrapper() -> Result<(),>
                   }
                   if usage_result == 0 && old_mem_value != stats.ru_maxrss
                   {
-                     eprintln!(" --  Max RSS {}", stats.ru_maxrss);
+                     eprintln!(" -- Max RSS {}", stats.ru_maxrss);
                      old_mem_value = stats.ru_maxrss;
                   }
                }
